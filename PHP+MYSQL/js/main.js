39be422013b2b086768dc71ee64ed25b3f4e51ec
Wellison Oliveira Silva <wonsilva100@gmail.com>
2022-10-21 10:00:11 -0300

    Initial commit

---
+++ PHP+MYSQL/js/main.js
@@ -0,0 +1,10 @@
+function clickMenu(){
+    if(itens.style.opacity == 0){
+    itens.style.opacity = 1;
+    itens.style.top = '75px';
+    
+    }else{
+        itens.style.opacity = 0;
+        itens.style.top = '-600px';
+    }
+}
---
+++ PHP+MYSQL/menu/tela1.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" href="../css/menustyle.css">
+
+
+    <title>Tela 1</title>
+</head>
+<body>
+
+        <header class="menu-nav-container">
+            <nav class="menu-nav">
+                <div class="menu-icon">
+                    <span class="material-icons" onclick="clickMenu()">menu</span>
+                </div>
+                <div class="menu-card-logo">
+                    <a href="#"><img src="../img/bebida.png" alt=""></a>
+                </div>
+            </nav>
+        </header>
+            <menu class="menu-card" id="itens">
+                <ul>
+                    <li><a href="#">vinho</a></li>
+                    <li><a href="#">vinho</a></li>
+                    <li><a href="#">vinho</a></li>
+                    <li><a href="#">vinho</a></li>
+                </ul>
+            </menu>
+        
+
+            <section>
+                <div class="menu-section">
+                    <div class="container">
+
+                    </div>
+                    <div class="container">
+
+                    </div>
+                    <div class="container">
+
+                    </div>
+                </div>
+                
+            </section>
+
+
+
+        
+    <script src="../js/main.js"></script>
+</body>
+</html>
